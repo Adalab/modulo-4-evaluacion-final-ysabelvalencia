cae12e6bfcf211328df5ae38c5fbb7f8731a65e6
ysabelvalencia <ysavalencia@hotmail.com>
2023-11-14 18:34:43 +0100

    endpoint to get all spells done and verified in postman

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ async function getConnection() {
     host: 'sql.freedb.tech',
     user: 'freedb_ysafvr8',
     password: 'a2ARTaqg69*zSA8',
-    database: 'reedb_module4_exam',
+    database: 'freedb_module4_exam',
   });
 
   connection.connect();
@@ -22,4 +22,22 @@ async function getConnection() {
 const port = 4500;
 app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
+});
+
+//1.ENDPOINT PARA COGER TODA LA LISTA DE SPELLS
+
+app.get('/spells', async (req, res) => {
+  let query = 'SELECT * FROM spells';
+
+  const conn = await getConnection();
+
+  const [spellsList] = await conn.query(query);
+
+  const numOfSpells = spellsList.length;
+
+  //Enviar una respuesta
+  res.json({
+    info: { count: numOfSpells }, // número de elementos
+    spellsList: spellsList, // listado
+  });
 });
