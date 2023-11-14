b9f2e11e5040569efc4197b62a0413cc219ee952
ysabelvalencia <ysavalencia@hotmail.com>
2023-11-14 17:14:42 +0100

    update bd

---
+++ createtablespells.sql
@@ -0,0 +1,29 @@
+use freedb_module4_exam;
+
+CREATE TABLE `spells` (
+  `idSpell` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `slug` varchar(60) NOT NULL,
+  `category` varchar(60) NOT NULL,
+  `creator` varchar(60) DEFAULT NULL,
+  `effect` varchar(60) NOT NULL,
+  `hand` varchar(60) DEFAULT NULL,
+  `image` varchar(150) DEFAULT NULL,
+  `incantation` varchar(60) DEFAULT NULL,
+  `light` varchar(60) NOT NULL
+);
+
+ALTER TABLE `freedb_module4_exam`.`spells` 
+ADD COLUMN `name` VARCHAR(60) NOT NULL AFTER `light`,
+ADD COLUMN `wiki` VARCHAR(150) NULL DEFAULT NULL AFTER `name`;
+
+
+INSERT INTO `spells` (`slug`, `category`, `creator`, `effect`, `hand`, `image`,`incantation`, `light`, `name`,`wiki`) VALUES ("age-line","Charm",null,"Prevents people above or below a certain age from access to a target",  null,"https://static.wikia.nocookie.net/harrypotter/images/e/e5/Age_Line_surrounding_the_Goblet_of_Fire_PM.jpg", null, "Blue","Age Line","https://harrypotter.fandom.com/wiki/Age_Line");
+
+ALTER TABLE `freedb_module4_exam`.`spells` 
+CHANGE COLUMN `effect` `effect` VARCHAR(150) NOT NULL ;
+
+INSERT INTO `spells` (`slug`, `category`, `creator`, `effect`, `hand`, `image`,`incantation`, `light`, `name`,`wiki`) VALUES ("alarte-ascendare","Charm",null,"Rockets target upward","Brandish wand","https://static.wikia.nocookie.net/harrypotter/images/c/c4/Alarte_Ascendare.gif","Alarte Ascendare(a-LAR-tay a-SEN-der-ay)","Red","Alarte Ascendare","https://harrypotter.fandom.com/wiki/Alarte_Ascendare");
+
+INSERT INTO `spells` (`slug`, `category`, `creator`, `effect`, `hand`, `image`,`incantation`, `light`, `name`,`wiki`) VALUES ("amplifying-charm","Charm",null,"Loudens target","Direct at target","https://static.wikia.nocookie.net/harrypotter/images/2/29/Sonorous_GOF_Dumbledore_1.jpg","Sonorus(soh-NOHR-us)","None","Amplifying Charm","https://harrypotter.fandom.com/wiki/Amplifying_Charm");
+
+
